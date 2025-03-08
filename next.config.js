dba523c3eccb3ea07bb14498b3d0c5cac8098e9b
Tanner Hallman <tannerhallman@users.noreply.github.com>
2025-03-08 12:26:35 -0700

    feat: add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+
+  async redirects() {
+    return [
+      {
+        source: "/redirect/livethemes",
+        destination: "https://livethemes.vercel.app",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
